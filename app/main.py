e8bc0fca926f361c8a0f68e8cc14c3ae7c457254
techybams <bamideleolaoluwa95@gmail.com>
2026-06-13 11:23:51 +0100

    main update

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.routers.users import router as user_router
 
-Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title="ISP Management API"
